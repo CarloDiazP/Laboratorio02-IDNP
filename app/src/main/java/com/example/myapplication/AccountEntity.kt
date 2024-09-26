223bc981daf0e12fb06ca3fe264a63b981c35dfe
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2024-09-25 20:24:38 -0500

    añadiendo AccountEntity

---
+++ app/src/main/java/com/example/myapplication/AccountEntity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+class AccountEntity (
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val phone: String,
+    val username: String,
+    val password: String
+) {
+
+
+}
