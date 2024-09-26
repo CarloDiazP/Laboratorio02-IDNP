97c47a09deb5301a4f783a771070c60bbf850b3b
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2024-09-25 21:00:37 -0500

    modificando y refactorizando LoginActivity

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,16 +1,12 @@
 package com.example.myapplication
 
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
