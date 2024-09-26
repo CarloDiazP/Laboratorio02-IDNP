b835b3ca45d4a4041a2d136a18d5dc94f7356078
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2024-09-25 21:54:46 -0500

    añadiendo binding y listener de form

--- app/src/main/java/com/example/myapplication/AccountActivity.kt
+++ app/src/main/java/com/example/myapplication/AccountActivity.kt
@@ -1,20 +1,40 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityAccountBinding
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class AccountActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_account)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btnOk = binding.btnOK
+        val btnCancel = binding.btnCancel
+
+        // listeners
+        btnOk.setOnClickListener {
+            val firstname = binding.edtFirstname.text.toString()
+            val lastname = binding.edtLastname.text.toString()
+            val email = binding.edtEmail.text.toString()
+            val phone = binding.edtPhone.text.toString()
+            val username = binding.edtUsername2.text.toString()
+            val password = binding.edtPassword2.text.toString()
+
+            val info = "Nombre: $firstname\nApellidos: $lastname\nCorreo: $email\nTelefono: $phone\nUsername: $username\nPassword: $password"
+
+            Toast.makeText(getApplicationContext(),"Cuenta registrada", Toast.LENGTH_SHORT).show()
+            Log.d("Register", info)
         }
     }
 }
